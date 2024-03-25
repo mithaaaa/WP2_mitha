28512dbc947ed28ab62f27b60d860fb496b95cf8
mithaaaa <januarizkiparamitha29@gmail.com>
2024-03-25 21:37:04 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Januarizki Paramitha
+              saya tinggal di daerah tambun";
+    }
+}
