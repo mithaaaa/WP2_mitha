b73c762a5f5f69b7ec1754e80ab289492b59a794
mithaaaa <januarizkiparamitha29@gmail.com>
2024-04-01 13:26:50 +0700

    Tugas Pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Januarizki</li>
+ <li>Nama Belakang : Paramitha</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Perum. Taman Raya Bekasi Blok L1 no.3</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Bogor</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ </ul>
+</section>
